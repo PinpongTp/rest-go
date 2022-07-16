3d20f4dc7cf9a35e9e5f46e363eb7726b8ebd395
PinpongTp <pinpong.tp@gmail.com>
2022-07-16 23:48:01 +0700

    connect firebase

--- go.mod
+++ go.mod
@@ -3,14 +3,35 @@ module pinpong.co/rest-go
 go 1.18
 
 require (
+	cloud.google.com/go/firestore v1.6.1
+	firebase.google.com/go v3.13.0+incompatible
 	github.com/gin-gonic/gin v1.8.1
+	github.com/mitchellh/mapstructure v1.5.0
+	google.golang.org/api v0.87.0
 	gorm.io/gorm v1.23.4
 )
 
 require (
+	cloud.google.com/go v0.102.0 // indirect
+	cloud.google.com/go/compute v1.7.0 // indirect
+	cloud.google.com/go/iam v0.3.0 // indirect
+	cloud.google.com/go/storage v1.22.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.1.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
+	github.com/googleapis/go-type-adapters v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20220622183110-fd043fe589d2 // indirect
+	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220624142145-8cd45d7dbd1f // indirect
+	google.golang.org/grpc v1.47.0 // indirect
 )
 
 require (
@@ -27,9 +48,9 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
-	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/sys v0.0.0-20220624220833-87e55d714810 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/driver/sqlite v1.3.6
--- main.go
+++ main.go
@@ -1,83 +1,192 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
+	"cloud.google.com/go/firestore"
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
+	"github.com/google/uuid"
+	"github.com/mitchellh/mapstructure"
+	"google.golang.org/api/iterator"
+	"pinpong.co/rest-go/models"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 )
 
-var db *gorm.DB
+func (route *App) createNoteHandler(c *gin.Context) {
+	var note models.Note
 
-type Handler struct {
-	db *gorm.DB
-}
-
-func newHandler(db *gorm.DB) *Handler {
-	return &Handler{db}
-}
-
-type Note struct {
-	ID      string `json:"id"`
-	Title   string `json:"title"`
-	Content string `json:"content"`
-}
-
-func (h *Handler) listNotesHandler(c *gin.Context) {
-	var notes []Note
-	if result := h.db.Find(&notes); result.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": result.Error.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusOK, &notes)
-}
-
-func (h *Handler) createNoteHandler(c *gin.Context) {
-	var note Note
 	if err := c.ShouldBindJSON(&note); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
-	if result := h.db.Create(&note); result.Error != nil {
+
+	id := uuid.New().String()
+	note.Id = id
+
+	var err error
+	_, _, err = route.client.Collection("notes").Add(route.ctx, &note)
+
+	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": result.Error.Error(),
+			"error": err,
 		})
 		return
 	}
+
 	c.JSON(http.StatusCreated, &note)
 }
 
-func (h *Handler) deleteNoteHandler(c *gin.Context) {
+func (route *App) findById(c *gin.Context) {
 	id := c.Param("id")
-	if result := h.db.Delete(&Note{}, id); result.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": result.Error.Error(),
+	NotesData := []models.Note{}
+
+	fmt.Printf(">> find by id: %v", id)
+	fmt.Println(id)
+	item := route.client.Collection("notes").Where("Id", "==", id).Documents(route.ctx)
+	for {
+		NoteData := models.Note{}
+		doc, err := item.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		mapstructure.Decode(doc.Data(), &NoteData)
+		NotesData = append(NotesData, NoteData)
+	}
+	c.JSON(http.StatusOK, NotesData)
+}
+
+type App struct {
+	//Router *mux.Router
+	client *firestore.Client
+	ctx    context.Context
+}
+
+func (route *App) Init() {
+	route.ctx = context.Background()
+	opt := option.WithCredentialsFile("my-test-firebase-e0b24-firebase-adminsdk-88aai-3b0e645857.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	route.client, err = app.Firestore(route.ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func (route *App) get(c *gin.Context) {
+	NotesData := []models.Note{}
+	item := route.client.Collection("notes").Documents(route.ctx)
+	for {
+		NoteData := models.Note{}
+		doc, err := item.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		mapstructure.Decode(doc.Data(), &NoteData)
+		NotesData = append(NotesData, NoteData)
+	}
+	c.JSON(http.StatusOK, NotesData)
+}
+
+func (route *App) updateNoteHandler(c *gin.Context) {
+	var note models.Note
+	if err := c.ShouldBindJSON(&note); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
 		})
 		return
 	}
-	c.Status(http.StatusNoContent)
+
+	if len(note.Id) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "not have id",
+		})
+		return
+	}
+
+	var refId string
+
+	// get Ref.ID for update
+	item := route.client.Collection("notes").Where("Id", "==", note.Id).Documents(route.ctx)
+	for {
+		doc, err := item.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		refId = doc.Ref.ID
+	}
+
+	// update
+	var err error
+	_, err = route.client.Collection("notes").Doc(refId).Set(route.ctx, note)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, note)
+	return
+}
+
+func (route *App) deleteNoteHandler(c *gin.Context) {
+	id := c.Param("id")
+	var refId string
+
+	// get Ref.ID for delete
+	item := route.client.Collection("notes").Where("Id", "==", id).Documents(route.ctx)
+	for {
+		doc, err := item.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		refId = doc.Ref.ID
+	}
+
+	// delete
+	var err error
+	_, err = route.client.Collection("notes").Doc(refId).Delete(route.ctx)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, "")
+	return
 }
 
 func main() {
-	var err error
-	db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	db.AutoMigrate(&Note{})
-
-	handler := newHandler(db)
+	route := App{}
+	route.Init()
 
 	r := gin.New()
-	r.GET("/notes", handler.listNotesHandler)
-	r.POST("/notes", handler.createNoteHandler)
-	r.DELETE("/notes/:id", handler.deleteNoteHandler)
+	r.GET("/notes", route.get)
+	r.GET("/notes/:id", route.findById)
+	r.POST("/notes", route.createNoteHandler)
+	r.PUT("/notes", route.updateNoteHandler)
+	r.DELETE("/notes/:id", route.deleteNoteHandler)
 	r.Run()
 }
