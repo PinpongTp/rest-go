2a1ffe28b4b3cb7d848ebcdc97e503dd0c079e8a
PinpongTp <pinpong.tp@gmail.com>
2022-07-17 16:39:20 +0700

    clean code usecase

--- usecases/notes.go
+++ usecases/notes.go
@@ -16,31 +16,31 @@ func NewNoteUseCase(noteRepo repositories.NoteRepository) *NoteUseCase {
 func (t *NoteUseCase) FindAll() (notes []models.Note, err error) {
 	var note []models.Note
 	note, err = t.noteRepo.FindAll()
-
+	//
 	return note, err
 }
 
 func (t *NoteUseCase) FindById(id string) (notes models.Note, err error) {
 	var note models.Note
 	note, err = t.noteRepo.FindByID(id)
-
+	//
 	return note, err
-}
-
-func (t *NoteUseCase) Delete(id string) (err error) {
-	err = t.noteRepo.DeleteById(id)
-
-	return err
 }
 
 func (t *NoteUseCase) Create(note models.Note) (notes models.Note, err error) {
 	note, err = t.noteRepo.Save(note)
-
+	//
 	return note, err
 }
 
 func (t *NoteUseCase) Update(note models.Note) (notes models.Note, err error) {
 	note, err = t.noteRepo.Save(note)
-
+	//
 	return note, err
+}
+
+func (t *NoteUseCase) Delete(id string) (err error) {
+	err = t.noteRepo.DeleteById(id)
+	//
+	return err
 }
