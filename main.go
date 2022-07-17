960da0246bfa274e473db4b521817e2732dcd99c
PinpongTp <pinpong.tp@gmail.com>
2022-07-17 18:46:05 +0700

    set port for deploy

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"pinpong.co/rest-go/deliveries/routes"
 )
 
 func main() {
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		fmt.Println("No port in heroku" + port)
+	}
 	r := routes.SetupRouter()
-	r.Run()
+	r.Run(":" + port)
 }
